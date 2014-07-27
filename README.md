8ba72e4f1a6277fc7a5aa0625f0c985cca719ea6
Philip Abbet <philip.abbet@gmail.com>
2014-07-27 14:36:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,131 @@
+# CASCExtractor
+
+A command-line tool to extract files from CASC (Content Addressable Storage Container)
+storages (used by Blizzard games since 2014).
+
+Works on MacOS X and Linux.
+
+
+## Dependencies
+
+The following libraries are necessary to build the extractor:
+
+* CascLib (http://www.zezula.net/en/casc/main.html), MIT license
+* SimpleOpt 3.4 (http://code.jellycan.com/simpleopt/), MIT License - part of the
+  distribution
+
+To download the CascLib submodule, do:
+
+    somewhere$ cd <path/to/the/source/of/CASCExtractor>
+    CASCExtractor$ git submodule init
+    CASCExtractor$ git submodule update
+
+
+## Compilation
+
+Requires <a href="http://www.cmake.org/">cmake</a> to build:
+
+    $ mkdir build
+    $ cd build
+    $ cmake <path/to/the/source/of/CASCExtractor>
+    $ make
+
+The executable will be put in build/bin/
+
+
+## Usage
+
+See `CASCExtractor --help` for more details. All the examples below are shown using the
+World of Warcraft: Warlords of Draenor Beta files, as of July 2014.
+
+Note that the CASC format doesn't store the name of the files in plain text, but only in a
+hashed version. Thus, a *listfile* is needed if you don't know the exact name of the file
+you want to extract. One such *listfile* for WoW:WoD is provided with CascLib in
+*CascLib/listfile/listfile-wow6.txt*.
+
+
+**Extract all the *.M2 files from a CASC storage:**
+
+    $ CASCExtractor -l /path/to/listfile-wow6.txt "/Applications/World of Warcraft Beta/Data/" *.M2
+    Opening '/Applications/World of Warcraft Beta/Data'...
+
+    Searching for '*.M2'...
+
+    Found files:
+      - Cameras\Abyssal_Maw_CameraFly_01.M2
+      - Cameras\Firelands_Bridge_Camera_01.M2
+      - Cameras\FlyBy_Maelstrom.M2
+      - Cameras\FlyBy_MoguBridge_Collapse.M2
+      - Cameras\FlyBy_MoguBridge_Collapse02.M2
+      - Cameras\FlyBy_MoguSecret_Door.M2
+      - Cameras\FlyBy_MoguSecret_Door02.M2
+      - Cameras\FlyByBloodElf.m2
+      - Cameras\FlyByDeathKnight.m2
+    ....
+
+
+**Extract a specific file from a CASC storage:**
+
+**IMPORTANT:** Note that the file name is enclosed in "". This is to prevent the shell
+to try to interpret the backslashes (\\) as the start of an escape sequence, which would
+result in invalid file names.
+
+    $ mkdir out
+    $ MPQExtractor -o out "/Applications/World of Warcraft Beta/Data/" "Cameras\Abyssal_Maw_CameraFly_01.M2"
+    Opening '/Applications/World of Warcraft Beta/Data'...
+
+    Extracting files...
+
+    $ ls out/
+    Abyssal_Maw_CameraFly_01.M2
+
+
+**Extract some specific files from a CASC storage, preserving the path hierarchy found
+inside the MPQ archive:**
+
+    $ mkdir out
+    $ MPQExtractor -f -o out "/Applications/World of Warcraft Beta/Data/" "Cameras\FlyBy_*.M2"
+    Opening '/Applications/World of Warcraft Beta/Data'...
+
+    Searching for 'Cameras\FlyBy_*.M2'...
+
+    Found files:
+      - Cameras\FlyBy_Maelstrom.M2
+      - Cameras\FlyBy_MoguBridge_Collapse.M2
+      - Cameras\FlyBy_MoguBridge_Collapse02.M2
+      - Cameras\FlyBy_MoguSecret_Door.M2
+      - Cameras\FlyBy_MoguSecret_Door02.M2
+      - Cameras\FlyByBloodElf.m2
+    ...
+
+    Extracting files...
+
+    $ ls out/Cameras/
+    FlyBy_Maelstrom.M2
+    FlyBy_MoguBridge_Collapse.M2
+    FlyBy_MoguBridge_Collapse02.M2
+    FlyBy_MoguSecret_Door.M2
+    FlyBy_MoguSecret_Door02.M2
+    FlyByBloodElf.m2
+    ...
+
+
+## License
+
+CASCExtractor is is made available under the MIT License. The text of the license is in
+the file 'LICENSE'.
+
+Under the MIT License you may use CASCExtractor for any purpose you wish, without warranty,
+and modify it if you require, subject to one condition:
+
+>   "The above copyright notice and this permission notice shall be included in
+>   all copies or substantial portions of the Software."
+
+In practice this means that whenever you distribute your application, whether as binary or
+as source code, you must include somewhere in your distribution the text in the file
+'LICENSE'. This might be in the printed documentation, as a file on delivered media, or
+even on the credits / acknowledgements of the runtime application itself; any of those
+would satisfy the requirement.
+
+Even if the license doesn't require it, please consider to contribute your modifications
+back to the community.
---
+++ main.cpp
@@ -0,0 +1,306 @@
+#include <CascLib.h>
+#include <SimpleOpt.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+
+using namespace std;
+
+
+struct tSearchResult
+{
+    string strFileName;
+    string strFullPath;
+};
+
+
+/**************************** COMMAND-LINE PARSING ****************************/
+
+// The valid options
+enum
+{
+    OPT_HELP,
+    OPT_LISTFILE,
+    OPT_SEARCH,
+    OPT_EXTRACT,
+    OPT_DEST,
+    OPT_FULLPATH,
+    OPT_LOWERCASE,
+};
+
+
+const CSimpleOpt::SOption COMMAND_LINE_OPTIONS[] = {
+    { OPT_HELP,             "-h",               SO_NONE    },
+    { OPT_HELP,             "--help",           SO_NONE    },
+    { OPT_LISTFILE,         "-l",               SO_REQ_SEP },
+    { OPT_LISTFILE,         "--listfile",       SO_REQ_SEP },
+    { OPT_SEARCH,           "-s",               SO_REQ_SEP },
+    { OPT_SEARCH,           "--search",         SO_REQ_SEP },
+    { OPT_EXTRACT,          "-e",               SO_REQ_SEP },
+    { OPT_EXTRACT,          "--extract",        SO_REQ_SEP },
+    { OPT_DEST,             "-o",               SO_REQ_SEP },
+    { OPT_DEST,             "--dest",           SO_REQ_SEP },
+    { OPT_FULLPATH,         "-f",               SO_NONE    },
+    { OPT_FULLPATH,         "--fullpath",       SO_NONE    },
+    { OPT_LOWERCASE,        "-c",               SO_NONE    },
+    { OPT_LOWERCASE,        "--lowercase",      SO_NONE    },
+    
+    SO_END_OF_OPTIONS
+};
+
+
+/********************************** FUNCTIONS *********************************/
+
+void showUsage(const std::string& strApplicationName)
+{
+    cout << "CASCExtractor" << endl
+         << "Usage: " << strApplicationName << " [options] <CASC_ROOT> <PATTERN>" << endl
+         << endl
+         << "This program can extract files from a CASC storage" << endl
+         << endl
+         << "Options:" << endl
+         << "    --help, -h:              Display this help" << endl
+         << "    --listfile <FILE>" << endl
+         << "    -l <FILE>                Use the list file FILE [required if PATTERN is not a full path]" << endl
+         << "    --dest <PATH>," << endl
+         << "    -o <PATH>:               The folder where the files are extracted (default: the" << endl
+         << "                             current one)" << endl
+         << "    --fullpath, -f:          During extraction, preserve the path hierarchy found" << endl
+         << "                             inside the storage" << endl
+         << "    --lowercase, -c:         Convert extracted file paths to lowercase" <<endl
+         << endl
+         << "Examples:" << endl
+         << endl
+         << "  1) Extract all the *.M2 files in a CASC storage:" << endl
+         << endl
+         << "       ./CASCExtractor -l listfile-wow6.txt \"/Applications/World of Warcraft Beta/Data/\" *.M2" << endl
+         << endl
+         << "  2) Extract a specific file from a CASC storage:" << endl
+         << "       IMPORTANT: The file name must be enclosed in \"\" to prevent the shell to" << endl
+         << "                  interpret the \\ character as the start of an escape sequence." << endl
+         << endl
+         << "       ./CASCExtractor -o out \"/Applications/World of Warcraft Beta/Data/\" \"Path\\To\\The\\File\"" << endl
+         << endl
+         << "  3) Extract some specific files from a CASC storage, preserving the path hierarchy" << endl
+         << "     found inside the MPQ archive:" << endl
+         << endl
+         << "       ./CASCExtractor -f -o out -l listfile-wow6.txt \"/Applications/World of Warcraft Beta/Data/\" \"Path\\To\\Extract\\*\"" << endl
+         << endl;
+}
+
+
+int main(int argc, char** argv)
+{
+    HANDLE hStorage;
+    string strListFile;
+    string strSearchPattern;
+    string strStorage;
+    string strDestination = ".";
+    vector<tSearchResult> searchResults;
+    bool bUseFullPath = false;
+    bool bLowerCase = false;
+
+
+    // Parse the command-line parameters
+    CSimpleOpt args(argc, argv, COMMAND_LINE_OPTIONS);
+    while (args.Next())
+    {
+        if (args.LastError() == SO_SUCCESS)
+        {
+            switch (args.OptionId())
+            {
+                case OPT_HELP:
+                    showUsage(argv[0]);
+                    return 0;
+
+                case OPT_LISTFILE:
+                    strListFile = args.OptionArg();
+                    break;
+
+                case OPT_DEST:
+                    strDestination = args.OptionArg();
+                    break;
+
+                case OPT_FULLPATH:
+                    bUseFullPath = true;
+                    break;
+
+                case OPT_LOWERCASE:
+                    bLowerCase = true;
+                    break;
+            }
+        }
+        else
+        {
+            cerr << "Invalid argument: " << args.OptionText() << endl;
+            return -1;
+        }
+    }
+
+    if (args.FileCount() != 2)
+    {
+        cerr << "Must specify both the path to a CASC storage and a search pattern" << endl;
+        return -1;
+    }
+
+    strStorage = args.File(0);
+    strSearchPattern = args.File(1);
+
+    // Remove trailing slashes at the end of the storage path (CascLib doesn't like that)
+    if ((strStorage[strStorage.size() - 1] == '/') || (strStorage[strStorage.size() - 1] == '\\'))
+        strStorage = strStorage.substr(0, strStorage.size() - 1);
+
+    cout << "Opening '" << strStorage << "'..." << endl;
+    if (!CascOpenStorage(strStorage.c_str(), 0, &hStorage))
+    {
+        cerr << "Failed to open the storage '" << strStorage << "'" << endl;
+        return -1;
+    }
+
+
+    // Search the files
+    if ((strSearchPattern.find("*") == string::npos) && (strSearchPattern.find("?") == string::npos))
+    {
+        tSearchResult r;
+        r.strFileName = strSearchPattern.substr(strSearchPattern.find_last_of("\\") + 1);
+        r.strFullPath = strSearchPattern;
+
+        searchResults.push_back(r);
+    }
+    else
+    {
+        if (strListFile.empty())
+        {
+            cerr << "No listfile specified, use the --listfile option" << endl;
+            return -1;
+        }
+
+        cout << endl;
+        cout << "Searching for '" << strSearchPattern << "'..." << endl;
+
+        CASC_FIND_DATA findData;
+        HANDLE handle = CascFindFirstFile(hStorage, strSearchPattern.c_str(), &findData, strListFile.c_str());
+    
+        if (handle)
+        {
+            cout << endl;
+            cout << "Found files:" << endl;
+
+            do {
+                cout << "  - " << findData.szFileName << endl;
+
+                tSearchResult r;
+                r.strFileName = findData.szPlainName;
+                r.strFullPath = findData.szFileName;
+
+                searchResults.push_back(r);
+            } while (CascFindNextFile(handle, &findData) && findData.szPlainName);
+
+            CascFindClose(handle);
+        }
+        else
+        {
+            cout << "No file found!" << endl;
+        }
+    }
+
+    // Extraction
+    if (!searchResults.empty())
+    {
+        char buffer[1000000];
+
+        cout << endl;
+        cout << "Extracting files..." << endl;
+        cout << endl;
+
+        if (strDestination.at(strDestination.size() - 1) != '/')
+            strDestination += "/";
+
+        vector<tSearchResult>::iterator iter, iterEnd;
+        for (iter = searchResults.begin(), iterEnd = searchResults.end(); iter != iterEnd; ++iter)
+        {
+            string strDestName = strDestination;
+
+            if (bUseFullPath)
+            {
+                if (bLowerCase){
+                    transform(iter->strFullPath.begin(), iter->strFullPath.end(), iter->strFullPath.begin(), ::tolower);
+                }
+
+                strDestName += iter->strFullPath;
+
+                size_t offset = strDestName.find("\\");
+                while (offset != string::npos)
+                {
+                    strDestName = strDestName.substr(0, offset) + "/" + strDestName.substr(offset + 1);
+                    offset = strDestName.find("\\");
+                }
+
+                offset = strDestName.find_last_of("/");
+                if (offset != string::npos)
+                {
+                    string dest = strDestName.substr(0, offset + 1);
+
+                    size_t start = dest.find("/", 0);
+                    while (start != string::npos)
+                    {
+                        string dirname = dest.substr(0, start);
+
+                        DIR* d = opendir(dirname.c_str());
+                        if (!d)
+                            mkdir(dirname.c_str(), S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+                        else
+                            closedir(d);
+
+                        start = dest.find("/", start + 1);
+                    }
+                }
+            }
+            else
+            {
+                if (bLowerCase){
+                    transform(iter->strFileName.begin(), iter->strFileName.end(), iter->strFileName.begin(), ::tolower);
+                }
+
+                strDestName += iter->strFileName;
+            }
+
+            HANDLE hFile;
+            if (CascOpenFile(hStorage, iter->strFullPath.c_str(), CASC_LOCALE_ALL, 0, &hFile))
+            {
+                DWORD read;
+                FILE* dest = fopen(strDestName.c_str(), "wb");
+                if (dest)
+                {
+                    do {
+                        if (CascReadFile(hFile, &buffer, 1000000, &read))
+                            fwrite(&buffer, read, 1, dest);
+                    } while (read > 0);
+
+                    fclose(dest);
+                }
+                else
+                {
+                    cerr << "Failed to extract the file '" << iter->strFullPath << "' in " << strDestName << endl;
+                }
+
+                CascCloseFile(hFile);
+            }
+            else
+            {
+                cerr << "Failed to extract the file '" << iter->strFullPath << "' in " << strDestName << endl;
+            }
+        }
+    }
+
+    CascCloseStorage(hStorage);
+
+    return 0;
+}
